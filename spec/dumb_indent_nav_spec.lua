5e2421021bb10254353733138d5bd4e6d52420e8
Fülöp Kovács <43729152+fulopkovacs@users.noreply.github.com>
2026-06-24 13:56:55 +0200

    use counts

--- spec/dumb_indent_nav_spec.lua
+++ spec/dumb_indent_nav_spec.lua
@@ -5,6 +5,8 @@ local lines = {
     "    // this is the greeting",
     "    const greeting = `Hello ${name}!`;",
     "    ",
+    "    const punctuation = '!';",
+    "    const excited = greeting .. punctuation;",
     "    return greeting;",
     "}",
 }
@@ -28,6 +30,9 @@ describe("same indent navigation", function()
 
     after_each(function()
         _G.print = original_print
+        pcall(vim.keymap.del, "n", "gn", {
+            buffer = 0,
+        })
     end)
 
     it("goes to the next line with the same indent", function()
@@ -44,14 +49,53 @@ describe("same indent navigation", function()
         }, vim.api.nvim_win_get_cursor(0))
     end)
 
+    it("goes to the nth next line with the same indent", function()
+        vim.api.nvim_win_set_cursor(0, {
+            2, 4,
+        })
+
+        assert.is_true(nav.goto_next_same_indent(3))
+        assert.are.same({
+            6, 4,
+        }, vim.api.nvim_win_get_cursor(0))
+    end)
+
+    it("goes to the nth previous line with the same indent", function()
+        vim.api.nvim_win_set_cursor(0, {
+            7, 4,
+        })
+
+        assert.is_true(nav.goto_prev_same_indent(3))
+        assert.are.same({
+            3, 4,
+        }, vim.api.nvim_win_get_cursor(0))
+    end)
+
+    it("uses v:count1 when called from a mapping", function()
+        vim.api.nvim_win_set_cursor(0, {
+            2, 4,
+        })
+        vim.keymap.set("n", "gn", function()
+            nav.goto_next_same_indent()
+        end, {
+            buffer = 0,
+        })
+
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("3gn", true, false, true), "xt", false)
+
+        assert.are.same({
+            6, 4,
+        }, vim.api.nvim_win_get_cursor(0))
+    end)
+
     it("prints a message when trying to go next from the last line", function()
         vim.api.nvim_win_set_cursor(0, {
-            6, 0,
+            8, 0,
         })
 
         assert.is_false(nav.goto_next_same_indent())
         assert.are.same({
-            6, 0,
+            8, 0,
         }, vim.api.nvim_win_get_cursor(0))
         assert.are.same({
             "Already at last line",
